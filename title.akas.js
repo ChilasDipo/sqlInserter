b6742bb24aa57fb961f0cab9ac3247d32fec6d7a
ChilasDipo <Chil0041@edu.easj.dk>
2022-03-29 09:24:57 +0200

    newest version

--- title.akas.js
+++ title.akas.js
@@ -44,22 +44,22 @@ lineReader.eachLine("akasData.tsv", function (line, last) {
 
   let akaconst = array[1]+array[0]
   
-  table.rows.add(
-    akaconst,
-    array[0],
-    parseInt(array[1]),
-    array[2],
-    array[3],
-    array[4],
-   stringBitToStringBoolean(array[7]),
-  );
+  // able.rows.add(
+  //   akaconst,
+  //   array[0],
+  //   parseInt(array[1]),
+  //   array[2],
+  //   array[3],
+  //   array[4],
+  //  stringBitToStringBoolean(array[7]),
+  // );
 
 
   if (array[5] != null) {
     let typesarray = array[5].split(",");
 
     for (let index = 0; index < typesarray.length; index++) {
-      types.rows.add(array[0], typesarray[index]);
+      types.rows.add(akaconst, typesarray[index]);
     }
   }
 
@@ -67,7 +67,7 @@ lineReader.eachLine("akasData.tsv", function (line, last) {
     let attributesarray = array[6].split(",");
 
     for (let index = 0; index < attributesarray.length; index++) {
-      attributes.rows.add(array[0], attributesarray[index]);
+      attributes.rows.add(akaconst, attributesarray[index]);
     }
   }
 
@@ -75,7 +75,7 @@ lineReader.eachLine("akasData.tsv", function (line, last) {
     insertData(table,types,attributes);
     counter = 0;
     countCounter++;
-    table = giveEmptyTable()[0];
+ //   table = giveEmptyTable()[0];
     types = giveEmptyTable()[1];
     attributes = giveEmptyTable()[2]
     if (last == true) {
@@ -126,7 +126,7 @@ async function insertData(table, types,attribute) {
       .connect()
       .then((pool) => {
         console.log("Connected to MSSQL");
-        pool.request().bulk(table);
+        //pool.request().bulk(table);
         pool.request().bulk(types);
         pool.request().bulk(attribute);
         return pool;
