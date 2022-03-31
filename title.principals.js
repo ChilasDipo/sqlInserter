fa8fda8201b58578840e8d53b831ea55e553a2d5
ChilasDipo <Chil0041@edu.easj.dk>
2022-03-31 18:43:55 +0200

    added comment to principals

--- title.principals.js
+++ title.principals.js
@@ -1,12 +1,11 @@
-
-
+//Importer Libaries som bliver brugt, et til at læse TSV filen, et til vores database koder og et til at snakke med databasen
 const lineReader = require('line-reader');
 const env = require("dotenv").config();
 const sql = require('mssql')
-
+//Counter for bulk insert den tæller hvor mange som er blevet sat ind
 let counter = 0;
-let bulkList = []
 
+//Sql object til at komunikere med databasen
 const sqlConfig = {
   user: process.env.user,
   password: process.env.password,
@@ -23,22 +22,24 @@ const sqlConfig = {
   }
 }
 
+//counter for antalet af gange, hvor der er sket et bulk insert
 let countCounter = 0;
 
+//Opsætter table hvor der kan sætte data ind i (See giveEmptyTable function for table stukture)
 let table = giveEmptyTable()
 
+//Read the TSV file LINE by LINE and Call the function for each line
 lineReader.eachLine('principalsData.tsv', function(line,last) {
 
+  //Checks if you are reading the first line of the TSV file and changes the line to be this temp line, witch will be removed in the database later (This could be done better with a akip line but that dident want to work)
   if ((countCounter == 0) & (counter == 0)) {
     console.log("First line is " + line);
     line = "tt0000000	0	nm0000000	test	\N	\N";
     console.log("First line is " + line);
   }
-
-
-
-
+// Counts each line 
   counter++
+//SPlit the line at each TAB and puts the values into an array  and changes the \N from the TSV files to NULL
   let array = line.split("\t");
       for (let index = 0; index < array.length; index++) {
         if (array[index] == "\\N") {
@@ -46,6 +47,7 @@ lineReader.eachLine('principalsData.tsv', function(line,last) {
         }
       }
 
+      //Puts the  values from the array into the table in the column where it fits
     table.rows.add(
       array[0],
       parseInt(array[1]),
@@ -55,13 +57,16 @@ lineReader.eachLine('principalsData.tsv', function(line,last) {
       array[5],
     );
       
+    //Checks if the counter is at 250000 or it is the last line 
     if (counter==250000 || last==true) {
+      //Gives the table to the insert function witch sends it to the database
       insertData(table)
+      //resets counter
       counter=0
      countCounter++
       table = giveEmptyTable()
       
-
+      //Checks if is the last line and ends the real line if it is the last one 
       if (last==true  ) {
         console.log('Done')
         return false
@@ -71,6 +76,7 @@ lineReader.eachLine('principalsData.tsv', function(line,last) {
 
 });
 
+//Function som opbygger sql tablen
 function giveEmptyTable(){
   let table = new sql.Table('Principals') // or temporary table, e.g. #temptable
     table.create = true
@@ -80,13 +86,12 @@ function giveEmptyTable(){
     table.columns.add("category", sql.NVarChar(sql.MAX), { nullable: true });
     table.columns.add("job", sql.NVarChar(sql.MAX), { nullable: true });
     table.columns.add("characters", sql.NVarChar(sql.MAX), { nullable: true });
-    
     return table
 }
 
 async function insertData(table){
-  try {
-  
+  try {  
+    //Makes the connection obejct for taking to the database and uses it to add the table to the database
 const poolPromise = new sql.ConnectionPool(sqlConfig)
   .connect()
   .then(pool => {
@@ -95,9 +100,6 @@ const poolPromise = new sql.ConnectionPool(sqlConfig)
     return pool
   })
   .catch(err => console.log('Database Connection Failed! Bad Config: ', err))
-
-
-
   }catch (error) {
     console.log(error)
     console.log(table)
